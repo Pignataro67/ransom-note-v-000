2a73ca4e6adc934b377cd70c277d2400e2a219b4
Pat  Pignataro <pat6284@msn.com>
2019-04-16 13:38:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,33 +23,3 @@ function buildHistogram(mag){
   }
   return hash
 }
-
-function binaryMatch(mag){
-
-}
-
-function canBuildNote(mag,note){
-  let hist = buildHistogram(mag)
-  for (let el of note){
-    if (hist[el] > 0){
-      hist[el]-=1
-    }
-    else{
-      return false
-    }
-  }
-  return true
-}
-
-function buildHistogram(mag){
-  let hash={}
-  for (let el of mag){
-    if (hash[el]){
-      hash[el]+=1
-    }
-    else{
-      hash[el]=1
-    }
-  }
-  return hash
-}
