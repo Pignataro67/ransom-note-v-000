72bc99ff64246c16d8595550ec88114528df418d
Pat  Pignataro <pat6284@msn.com>
2019-04-16 13:32:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-function buildHistogram(arr) {
-  let histogram = new Map();
+// function buildHistogram(arr) {
+//   let histogram = new Map();
 
-  arr.forEach(n => {
-    histogram.get(n) ? histogram.set(n, histogram.get(n) + 1) : histogram.set(n, 1);
-  });
+//   arr.forEach(n => {
+//     histogram.get(n) ? histogram.set(n, histogram.get(n) + 1) : histogram.set(n, 1);
+//   });
 
-  return histogram;
-}
-
-// function canBuildNote(arr, note) {
-//   const magazineHistogram = buildHistogram(arr);
-//   const noteHistogram = buildHistogram(note.split(''));
-
-//   for (let [char, count] of noteHistogram) {
-//     const magCharCount = magazineHistogram.get(char) || 0;
-//     if (count > magCharCount) { return false; }
-//   }
-
-//   return true;
+//   return histogram;
 // }
 
+// function canBuildNote(mag,note){
+//   let hist = buildHistogram(mag)
+//   for (let el of note){
+//     if (hist[el] > 0){
+//       hist[el]-=1
+//     }
+//     else{
+//       return false
+//     }
+//   }
+//   return true
+// }
 
-
-function canBuildNote(mag,note){
-  let hist = buildHistogram(mag)
-  for (let el of note){
-    if (hist[el] > 0){
-      hist[el]-=1
-    }
-    else{
-      return false
-    }
-  }
-  return true
-}
-
-function buildHistogram(mag){
-  let hash={}
-  for (let el of mag){
-    if (hash[el]){
-      hash[el]+=1
-    }
-    else{
-      hash[el]=1
-    }
-  }
-  return hash
-}
+// function buildHistogram(mag){
+//   let hash={}
+//   for (let el of mag){
+//     if (hash[el]){
+//       hash[el]+=1
+//     }
+//     else{
+//       hash[el]=1
+//     }
+//   }
+//   return hash
+// }
